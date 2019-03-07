0312a53efe4654a300532bfa15d94f0997b5a2ca
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-07 12:06:18 +0200

    Added KirjaLuokka

---
+++ KirjaLuokka/KirjaLuokka/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KirjaLuokka
+{
+   class Book
+   {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        public Book(string title, string author, string id, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Kirja: {this.title}\n" +  $"kirjoittaja{this.author}\n" +
+                $"id: {this.id}\n" + $"Hinta: {this.price.ToString()}\n)");
+        }
+        public string CompareBooks(Book book)
+        {
+          Console.WriteLine($"Kirja: {book.title} hinta: {this.price.ToString()}\n");
+
+            if (this.price > book.price)
+            {
+                Console.WriteLine("*********************************************************\n");
+                return $"{book.title} kirja on kalliimpi kuin {this.title} kirja\n";
+            }
+            else
+            {
+                Console.WriteLine("*********************************************************\n");
+                return $"{this.title} kirja on kalliimpi kuin {book.title} kirja.\n*********************************************************";
+       
+            }
+        }
+    }
+}
+
+
+
+
+
+
---
+++ KirjaLuokka/KirjaLuokka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using KirjaLuokka;
+
+namespace KirjaLuokka
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*Kirjat ja hinnat*-*-*-*-*-*-*-*-*-*-");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+            Book book1 = new Book("Jere", "Aki Linnanahde", "0001", 27.50);          
+            Book book2 = new Book("C# Ohjelmointi", "Ghodrat moghadampour", "0003", 22.50);
+
+            book1.CompareBooks(book2);
+            Console.WriteLine(book2.CompareBooks(book1));
+        }
+    }
+
+
+}
