35e279cdcf960d81b89f710c567f3933c3588552
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-07 12:50:10 +0200

    Added Poikkeuskäsittely

---
+++ Poikkeuskasittely/ExceptionHandling/ExceptionHandling/FileManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    public class WordListNotFoundException : Exception
+    {
+        public WordListNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public WordListNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+
+    class FileManager
+    {
+        public string filePath;
+
+        public FileManager()
+        {
+        }
+
+        public FileManager(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public string ReadWords()
+        {
+            string content = string.Empty;
+
+            try
+            {
+                content = ReadFile();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error:{e.Message}");
+            }
+
+            return content;
+        }
+
+        private string ReadFile()
+        {
+            if (File.Exists(filePath))
+            {
+
+                List<Movie> movielList = JsonConvert.DeserializeObject<List<Movie>>(File.ReadAllText(this.filePath));
+
+
+                foreach (Movie movie in movielList)
+                {
+                    Console.WriteLine($"Name: {movie.Name}\n Year: {movie.Year}");
+                }
+            }
+
+            else if  (!File.Exists(filePath))
+                {
+                    throw new WordListNotFoundException("file not available");
+                }
+
+
+            
+                string directoryName = Path.GetDirectoryName(filePath);
+                string fileName = Path.GetFileName(filePath);
+                string fileExtension = Path.GetExtension(filePath);
+
+                Console.WriteLine("directoryName: " + directoryName);
+                Console.WriteLine("fileName: " + fileName);
+                Console.WriteLine("fileExtension: " + fileExtension);
+
+                string fileContent = File.ReadAllText(filePath, Encoding.UTF8);
+                return fileContent;
+            
+
+
+
+        }
+    }
+}
+        
---
+++ Poikkeuskasittely/ExceptionHandling/ExceptionHandling/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    
+    }
+
+
+}
