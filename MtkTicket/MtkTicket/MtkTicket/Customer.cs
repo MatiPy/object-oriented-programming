7b84c60d19b75dabc58f1e106d8c685aefa64e23
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-07 13:03:07 +0200

    Added MtkTicket

---
+++ MtkTicket/MtkTicket/MtkTicket/Customer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MtkTicket
+{
+    class Customer
+    {
+        private readonly string name = null;
+        private bool student = false;
+        private bool milPerson = false;
+        private bool mtkMbr = false;
+
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name {
+            get {
+                return this.name;
+            }
+        }
+        public int Age
+        {
+            get; set; } = 0;
+
+        public bool Student
+        {
+            get { return student; } set { student = value; }
+        }
+        public bool MtkMbr
+        {
+            get { return mtkMbr; }
+            set { mtkMbr = value; }
+        }
+        public bool MilPerson
+        { get { return milPerson; }
+            set { milPerson = value; }
+        }
+        private const double normalPrice = 16;             //peruslippu 16e 
+3  
+        public double SearchForPrice()
+        {
+            double price = normalPrice;  
+
+            if (Age < 7)
+            {
+                price = 0;
+                Console.WriteLine("\n      Tervetuloa! Sinulle on ilmainen sisäänpääsy");
+            }
+            else if (Age  >= 65)
+            {
+                price *= 0.50;
+                Console.WriteLine("\n        Hei Eläkeläinen saat lippusi puoleen hintaan");
+            }  
+            else if (mtkMbr && Student)
+            {
+                price *= 0.40; Console.WriteLine("\nOlet Jäsen sekä opiskelija!");
+            }
+            else if (Age <= 15)
+            {
+                price *= 0.50;
+            }
+            else if (MilPerson)
+            {
+                price *= 0.50;
+            }
+            else if (Student)
+            {
+                price *= 0.55;
+            }
+            else if (MtkMbr)
+            {
+                price *= 0.85;
+                Console.WriteLine("\n  " + "                 *** Hei jäsenemme ***");
+            }
+            else
+            {
+
+            }
+            return price;
+        }
+    }
+}
---
+++ MtkTicket/MtkTicket/MtkTicket/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MtkTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------Pohjustetaan tiedot maatalousnäyttelyn oikean lipun valintaan-------------------");
+            Console.Write("-------------------------------Kerro lippumaatille nimesi---------------------------------------\n");
+            var name = Console.ReadLine();
+            var customer = new Customer(name);
+            int age = -1;
+
+            while (age < 0)
+            {
+                Console.Write("Syötä ikäsi: ");
+                if 
+                    (!int.TryParse(Console.ReadLine(), out age))
+                {
+                    Console.WriteLine("Väärä syöte!");
+                    age = -1;
+                }
+                else
+                {
+                    customer.Age = age;
+                }
+            }
+            Console.WriteLine("\nOhjelma Kysyy sinulta seuraavaksi tietoja lipun ostoa varten....\n"
+                + "             Paina jotakin jatkaaksesi\n****************************************************************\n");
+            Console.ReadKey();
+
+            customer.Student = YesNo
+                ("\nKerro oletko Opiskelija?\n");
+            customer.MtkMbr = YesNo
+                ("\nKerro oletko MTK:n jäsen?\n");
+            //kysytään käyttäjältä tiedot  
+            customer.MilPerson = YesNo("\nKerro oletko Varusmies?\n"); 
+            
+            Console.WriteLine($"\n**************Sinun Lippusi hinta {name} on****************\n {customer.SearchForPrice()} Euroa");
+            Console.ReadKey();
+        }
+        private static bool YesNo(string ask)
+        {
+            while (true)
+            {
+                Console.Write("\n{0}Näppäin[K]=Kyllä -- [E]=Ei:", ask);
+                char c = char.ToUpper(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+                
+                if (c == 'K')
+                {
+                    return true;
+                }
+                if (c == 'E')
+                {
+                    return false;
+                }
+                Console.WriteLine("Vastaa seuraavasti [K=Kyllä] [E=Ei]");
+            }
+        }
+    }
+}
